0a49f3f166045dfec10d7eac81ffffad2c21c13d
niravron <avron.nir@gmail.com>
2023-03-14 11:49:07 +0200

    new

--- scrape.py
+++ scrape.py
@@ -17,7 +17,7 @@ HTML_TEMPLATE = '''
 '''
 
 
-response = requests.get("https://en.wikipedia.org/wiki/Cristiano_Ronaldo")
+response = requests.get("https://en.wikipedia.org/wiki/Roger_Federer")
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, "html.parser")
